e93adaa354bebe0f18e59bec6119c96ed8ba0948
ShamanRB <52468647+ShamanRB@users.noreply.github.com>
2020-01-17 21:22:13 +0300

    Add files via upload

---
+++ lab6/src/main/java/Lab6/lab6/App.java
@@ -0,0 +1,13 @@
+package Lab6.lab6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
